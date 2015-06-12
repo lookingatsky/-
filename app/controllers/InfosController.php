cf7871bc63466e1feccdffe88c5b465aaf42d838
钟浩林 <zhonghaolin@zailicai.com>
2015-06-12 14:51:42 +0800

    去掉infosController的fb

--- app/controllers/InfosController.php
+++ app/controllers/InfosController.php
@@ -25,8 +25,7 @@ class InfosController extends ControllerBase
 			 $this->flash->error("请不要乱操作！");
 			 return $this->forward("index/index");
 		}else{
-			$getInfos = BaiduApiInfo::FindFirst("uid = '".$id."'");
-			fb($getInfos);
+			$getInfos = BaiduApiInfo::FindFirst("uid = '".$id."'");\
 			$this->view->infos = $getInfos;			
 		}	
     }
